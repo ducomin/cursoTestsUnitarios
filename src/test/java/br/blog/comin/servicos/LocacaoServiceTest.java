28666576e0c6c9f2016a58952eb95257bc7b01a0
Eduardo Comin <eduardo.comin@capgemini.com>
2023-09-22 10:48:37 -0300

    Testando métodos privados diretamente - Aulas 39

--- src/test/java/br/blog/comin/servicos/LocacaoServiceTest.java
+++ src/test/java/br/blog/comin/servicos/LocacaoServiceTest.java
@@ -39,6 +39,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import br.blog.comin.dao.EmailService;
 import br.blog.comin.dao.LocacaoDAO;
@@ -257,5 +258,17 @@ public class LocacaoServiceTest {
 		//verificacao
 		Assert.assertThat(locacao.getValor(), is(1.0));
 		PowerMockito.verifyPrivate(service).invoke("getValorTotal", filmes);
+	}
+
+	@Test
+	public void deveCalcularValorLocacao() throws Exception{
+		//cenario
+		List<Filme> filmes = Arrays.asList(umFilme().agora());
+
+		//acao
+		Double valor = (Double) Whitebox.invokeMethod(service, "getValorTotal", filmes);
+
+		//verificacao
+		Assert.assertThat(valor, is(4.0));
 	}
 }
